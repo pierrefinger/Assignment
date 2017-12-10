ccf95505480371214e7f771a8c400c4c9d959f16
pierrefinger <32704491+pierrefinger@users.noreply.github.com>
2017-12-10 17:53:59 +0100

    commit3
    -added 2 paddles
    -movment for paddle/playey using keyboard implmented

---
+++ Assignment_Pong/Assets/Scripts/player_1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_1 : MonoBehaviour {
+    public float speed = 5.0f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+}
